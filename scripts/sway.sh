82773cec7c459339714c9a6fe6494db91623eb17
colby <colby.braden@gmail.com>
2024-02-14 07:39:36 -0600

    added initial configs and dotfiles

---
+++ scripts/sway.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo pacman -S --needed sway swaylock swayidle swaybg firefox i3status \
+    foot rofi seatd playerctl arc-gtk-theme papirus-icon-theme \
+    lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db neofetch \
+    gtk-engine-murrine mpd mpv xorg-xwayland gvfs file-roller lxsession \
+    gamemode flatpak polkit cups-dinit ipp-usb ghostscript dunst imv libadwaita \
+    gnome-themes-extra otf-font-awesome ttf-croscore ttf-dejavu ttf-droid \
+    gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family
